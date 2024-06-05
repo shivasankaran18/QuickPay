6310d11a3c8730f2e8c9665ac70be951a31d2ad6
shiva <shiva_05@outlook.com>
2024-06-05 21:14:20 +0530

    update-12

--- apps/user-app/components/MerchantCard.tsx
+++ apps/user-app/components/MerchantCard.tsx
@@ -13,7 +13,7 @@ import { getMerchants, getUsers } from "../app/lib/action";
 import { Spinner } from "@repo/ui/spinner";
 import { Toast } from "@repo/ui/toast";
 
-const BACKEND_URL="http://ec2-16-171-197-171.eu-north-1.compute.amazonaws.com:3001"
+const BACKEND_URL="http://ec2-16-171-169-198.eu-north-1.compute.amazonaws.com:3001"
 
 
 
